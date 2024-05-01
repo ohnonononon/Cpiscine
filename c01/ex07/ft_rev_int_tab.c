e9a11bad3ca28fb8e669e08067a130c80972ab77
Nicolas Maturana Chandia <nimatura@cbr9s2.42barcelona.com>
2024-05-01 11:26:42 +0200

    fix: ex07: if inverts past size/2 it reallocates back to original place

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nimatura <nimatura@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 11:04:46 by nimatura          #+#    #+#             */
+/*   Updated: 2024/05/01 11:25:51 by nimatura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 void	ft_rev_int_tab(int *tab, int size)
 {
 	int	tmp;
 	int	i;
 
 	i = 0;
-	while (i < size - 1)
+	while (i < size / 2)
 	{
 		tmp = tab[i];
-		tab[i] = tab[size - 1];
-		tab[size - 1] = tmp;
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = tmp;
 		i++;
 	}
 }
