d24202b96b06a8f0cb8c126dc9a70d03447f8ef6
Nicolas Maturana Chandia <nimatura@cbr1s3.42barcelona.com>
2024-05-02 20:51:28 +0200

    strlcpy iterates while i < size - 1 && i < src_len. Cant iterate until dst_len since it could be set as nulls

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,34 +6,53 @@
 /*   By: nimatura <nimatura@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 17:10:28 by nimatura          #+#    #+#             */
-/*   Updated: 2024/05/02 17:10:49 by nimatura         ###   ########.fr       */
+/*   Updated: 2024/05/02 20:50:33 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//#include <string.h>
+//#include <stdio.h>
 
 unsigned int	ft_strlen(char *str)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (*(str + i) != '\0')
+	while (str[i] != '\0')
 		i++;
 	return (i);
 }
 
 unsigned int	ft_strlcpy(char *dst, char *src, unsigned int size)
 {
-	int	i;
-	int	src_len;
-	int	dst_len;
+	unsigned int	i;
+	unsigned int	src_len;
+	unsigned int	dst_len;
 
 	i = 0;
 	src_len = ft_strlen(src);
-	while (i < size - 2)
+	dst_len = ft_strlen(dst);
+	if (size == 0)
+		return (src_len);
+	while (i < size - 1 && i < src_len)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	if (size)
-		dst[size - 1] = '\0';
+	dst[i] = '\0';
 	return (src_len);
 }
+//
+//int	main()
+//{
+//	char	s1[] = "123456789";
+//	char	s2[2];
+//	char	s3[2];
+//	char	s1len;
+//
+//	s1len = ft_strlen(s1);
+//	printf("test:	%s	%u\n\n", s1, s1len);
+//	printf("og:	%s	%zu\n", s2, strlcpy(s2, s1, s1len));
+//	printf("ft:	%s	%u\n", s3, ft_strlcpy(s3, s1, s1len));
+//	return (0);
+//}
