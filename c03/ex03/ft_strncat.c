ad4a9df785a8c143a3446ea2a5526c5660ad8b5b
Nicolas Maturana Chandia <nimatura@cbr2s6.42barcelona.com>
2024-05-06 14:22:30 +0200

    ex03 init and end

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nimatura <nimatura@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 14:17:00 by nimatura          #+#    #+#             */
+/*   Updated: 2024/05/06 14:22:13 by nimatura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i = i + 1;
+	return (i);
+}
+
+char	*ft_strncat(char *dst, char *src, unsigned int nb)
+{
+	unsigned int	src_len;
+	unsigned int	dst_len;
+	unsigned int	i;
+
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+	i = 0;
+	if (nb == 0)
+		return (dst);
+	while (i < src_len && i < nb)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	return (dst);
+}
