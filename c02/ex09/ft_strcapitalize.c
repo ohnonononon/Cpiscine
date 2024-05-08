5089ca33b29c68f634eb8e0a05e2a8b57472455a
Nicolas Maturana Chandia <nimatura@cbr14s2.42barcelona.com>
2024-05-08 22:04:34 +0200

    fixgit add ft_strcapitalize.cgit add ft_strcapitalize.c

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: nimatura <nimatura@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 17:05:09 by nimatura          #+#    #+#             */
-/*   Updated: 2024/05/06 13:44:53 by nimatura         ###   ########.fr       */
+/*   Updated: 2024/05/08 22:03:37 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@ int	aux_alphanum(char c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	if (c >= '0' && c <= '9')
+	else if (c >= '0' && c <= '9')
 		return (1);
-	if (c >= 'A' && c <= 'Z')
+	else if (c >= 'A' && c <= 'Z')
 		return (1);
 	return (0);
 }
@@ -28,13 +28,12 @@ char	*ft_strcapitalize(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		while (aux_alphanum(str[i]) == 0 && str[i] != '\0')
+		while (!aux_alphanum(str[i]) && str[i] != '\0')
 			i++;
 		if (str[i] >= 'a' && str[i] <= 'z')
-		{
 			str[i] -= 32;
+		if (str[i] != '\0')
 			i++;
-		}
 		while (aux_alphanum(str[i]) == 1 && str[i] != '\0')
 		{
 			if (str[i] >= 'A' && str[i] <= 'Z')
