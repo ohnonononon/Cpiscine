ef370de5a3043e918075602139e7d75387e46340
ohnonononon <maturana.hablando@gmail.com>
2024-05-11 22:21:12 +0200

    init print program name

---
+++ c06/ex00/ft_print_program_name.c
@@ -0,0 +1,21 @@
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int ac, char **av)
+{
+	char	*str;
+
+	str = av[0];
+	ft_putstr(str);
+	return (0);
+}
