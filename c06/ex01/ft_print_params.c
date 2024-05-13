5c020f8750c260b824e0a657ac5ab55390e3d0b0
Nicolas Maturana Chandia <nimatura@cbr8s5.42barcelona.com>
2024-05-13 13:35:21 +0200

    print_params init

---
+++ c06/ex01/ft_print_params.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nimatura <nimatura@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 13:34:50 by nimatura          #+#    #+#             */
+/*   Updated: 2024/05/13 13:34:52 by nimatura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, str + i, 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int ac, char **av)
+{
+	int		i;
+
+	i = 1;
+	if (ac < 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	while (i <= ac - 1)
+	{
+		ft_putstr(av[i]);
+		i++;
+	}
+	return (0);
+}
