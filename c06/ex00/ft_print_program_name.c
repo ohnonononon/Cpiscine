4a1a8b9250769d2de5077eecb7877769c9f92061
Nicolas Maturana Chandia <nimatura@cbr14s2.42barcelona.com>
2024-05-13 21:01:43 +0200

    este si

--- c06/ex00/ft_print_program_name.c
+++ c06/ex00/ft_print_program_name.c
@@ -6,30 +6,27 @@
 /*   By: nimatura <nimatura@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 13:24:04 by nimatura          #+#    #+#             */
-/*   Updated: 2024/05/13 13:29:16 by nimatura         ###   ########.fr       */
+/*   Updated: 2024/05/13 19:01:23 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0
-	while (str[i])
-	{
-		write(1, str + i, 1);
-		i++;
-	}
-	write(1, "\n", 1);
-}
-
 int	main(int ac, char **av)
 {
+	int	i;
 	char	*str;
 
-	str = av[0];
-	ft_putstr(str);
+	i = 0;
+	str = *av;
+	if (ac > 0)
+	{
+		while (str[i] != '\0')
+		{
+			write(1, str + i, 1);
+			i++;
+		}
+		write(1, "\n", 1);
+	}
 	return (0);
 }
